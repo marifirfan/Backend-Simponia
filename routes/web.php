e12ce0f3fdd19349672c3c2754e10f0694bba386
Muhammad Arif Irfan <marifirfannn@gmail.com>
2024-04-27 14:14:56 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/about', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return view('landingpage');
+});
+
+Route::get('/page', function () {
+    return view('landingpage');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
