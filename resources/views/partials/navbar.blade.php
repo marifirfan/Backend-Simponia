4590a50fcdab4a941dbb29e87e872ab2904d03dc
Muhammad Arif Irfan <marifirfannn@gmail.com>
2024-04-27 14:56:37 +0700

    add new navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,24 +1,41 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="home">SIMPONIA</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Portofolio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Showcase</a>
-          </li>
-        </ul>
-        <span class="navbar-text">
-          Navbar text with an inline element
-        </span>
-      </div>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="dashboard">SIMPONIA</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Portofolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Showcase</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Detail Account</a>
+        </li>
+      </ul>
     </div>
-  </nav>
+    <!-- Menempatkan "Nama Mahasiswa" dan "Logout" di sebelah kanan -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Nama Mahasiswa</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<!-- CSS untuk memisahkan dan menempatkan ke kanan -->
+<style>
+.navbar-nav.right {
+  margin-left: auto;
+}
+</style>
+
