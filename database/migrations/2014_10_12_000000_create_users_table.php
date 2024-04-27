8125266204f84934492a97a4c9a77073f6013452
Muhammad Arif Irfan <marifirfannn@gmail.com>
2024-04-27 15:36:57 +0700

    update database migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('nim')->unique();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
