a2131013961297b33ef779c0b84e031e7d69ab96
fitraaromeo <fitraromeowinky@gmail.com>
2024-04-28 15:08:33 +0700

    first commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login to Simponia - Landing Page</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <!-- Custom CSS -->
+  <style>
+    /* Custom styles */
+    body {
+      font-family: 'Montserrat', sans-serif;
+      background: radial-gradient(circle, #101D37, #1B3E84);
+      color: #15264A;
+    }
+    .login-container {
+      background-color: rgba(255, 255, 255, 0.9);
+      border-radius: 10px;
+      padding: 20px;
+      max-width: 400px;
+      margin: auto;
+      margin-top: 50px;
+    }
+    .login-container h2 {
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="ml-auto">
+      <a class="btn btn-secondary" href="page">Back</a>
+    </div>
+  </nav>
+
+  <!-- Login Form -->
+  <div class="container login-container">
+    <h2>Register to Simponia!</h2>
+    <form action="/register" method="POST">
+      <div class="form-group">
+        <label for="username">NIM / USERNAME</label>
+        <input type="text" class="form-control" id="username" placeholder="Enter Nim" autofocus required>
+      </div>
+      <div class="form-group">
+        <label for="password">PASSWORD</label>
+        <input type="password" class="form-control" id="password" placeholder="Enter Pic" required>
+      </div>
+      <button type="submit" class="btn btn-primary btn-block">Register</button>
+    </form>
+  </div>
+
+  <!-- Bootstrap JS (Optional) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
